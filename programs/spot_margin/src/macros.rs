18ec90b1dee12ba3f870ab0f810aadb6ba8544a6
Karthik Pariti <karthik@spedx.exchange>
2023-08-02 22:43:26 +0530

    validation macro

--- programs/spot_margin/src/macros.rs
+++ programs/spot_margin/src/macros.rs
@@ -1,3 +1,5 @@
+// use crate::error::ErrorCode;
+
 #[macro_export]
 macro_rules! load {
     ($account_loader: expr) => {{
@@ -5,5 +7,28 @@ macro_rules! load {
             let error_code = ErrorCode::UnableToLoadAccountLoader;
             msg!("Error {} thrown at {}:{}", error_code, file!(), line!());
         })
+    }};
+}
+
+#[macro_export]
+macro_rules! validate {
+    ($assert:expr, $err:expr) => {{
+        if ($assert) {
+            Ok(())
+        } else {
+            let error_code: ErrorCode = $err;
+            msg!("Error {} thrown at {}:{}", error_code, file!(), line!());
+            Err(error_code)
+        }
+    }};
+    ($assert:expr, $err:expr, $($arg:tt)+) => {{
+        if($assert) {
+            Ok(())
+        } else{
+            let error_code: ErrorCode = $err;
+            msg!("Error {} thrown at {}:{}", error_code, file!(), line!());
+            msg!($($arg)*);
+            Err(error_code);
+        }
     }};
 }
