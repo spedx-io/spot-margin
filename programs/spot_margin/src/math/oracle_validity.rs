4ca195a6b0f26167ec37a177e97db35c1289dc8b
Karthik Pariti <karthik@spedx.exchange>
2023-08-11 21:23:44 +0530

    add blockers for certain actions, and fetch oracle status with lifetimes

--- programs/spot_margin/src/math/oracle_validity.rs
+++ programs/spot_margin/src/math/oracle_validity.rs
@@ -1,10 +1,10 @@
 use std::cmp::max;
 
-use borsh::{
-    BorshSerialize,
-    BorshDeserialize
-};
-use solana_program::msg;
+// use borsh::{
+//     BorshSerialize,
+//     BorshDeserialize
+// };
+// use solana_program::msg;
 
 use crate::{
     error::{
@@ -13,11 +13,15 @@ use crate::{
     },
     math::{
         casting::Cast,
-        constants::BID_ASK_SPREAD_PRECISION,
+        constants::{
+            BID_ASK_SPREAD_PRECISION,
+            PERCENTAGE_PRECISION_U64,
+            BID_ASK_SPREAD_PRECISION_I128
+        },
         safe_math::SafeMath,
     },
     state::{
-        oracle::OraclePriceData,
+        oracle::{OraclePriceData, HistoricalPriceData},
         enums::{
             MarketStatus,
             OracleValidity,
@@ -25,10 +29,16 @@ use crate::{
         },
         guard_rails::{
             OracleGuardRails,
-            ValidityGuardRails
-        }
+            ValidityGuardRails,
+            PriceDivergenceGuardRails
+        },
+        market::Market
     }
 };
+
+// use super::constants::BID_ASK_SPREAD_PRECISION_I128;
+
+// use super::constants::{PERCENTAGE_PRECISION, PERCENTAGE_PRECISION_U64};
 
 /// Function to check if the oracle data is valid for a specific action in the protocol
 /// Such as filling an order, adding an order, etc.
@@ -78,6 +88,8 @@ pub fn is_oracle_valid_for_action(
     Ok(is_action_valid)
 }
 
+/// Function to check the oracle validity. Returns the OracleValidity enum, with different outcomes ranging from descending
+/// order of severity of invalidity. 
 pub fn oracle_validity(
     last_oracle_twap: i64,
     oracle_price_data: &OraclePriceData,
@@ -125,6 +137,7 @@ pub fn oracle_validity(
         return Err(ErrorCode::OracleConfidenceTooWide.into())
     }
 
+    // Returns true if the oracle price is stale for margin calculations
     let is_oracle_price_stale_for_margin = oracle_delay.gt(
         &oracle_validity_guard.slots_before_stale_for_margin
     );
@@ -133,6 +146,8 @@ pub fn oracle_validity(
         return Err(ErrorCode::OraclePriceStaleForMargin.into())
     }
 
+    // If else if else loop returning different outcomes of the OracleValidity enum based on different oracle
+    // results
     let oracle_validity = if is_oracle_price_negative {
         OracleValidity::Invalid
     } else if is_oracle_price_too_volatile {
@@ -148,4 +163,115 @@ pub fn oracle_validity(
     };
 
     Ok(oracle_validity)
+}
+
+/// Function to block an action(Action)
+pub fn block_action(
+    market: &Market,
+    oracle_price_data: &OraclePriceData,
+    guard_rails: &OracleGuardRails,
+    last_acceptable_price: Option<u64>,
+    historical_oracle_data: &HistoricalPriceData
+) -> SpedXSpotResult<bool> {
+    let OracleStatus {
+        oracle_validity,
+        is_mark_price_too_divergent,
+        oracle_mark_spread_pct: _,
+        ..
+    } = get_oracle_status(
+        oracle_price_data, 
+        guard_rails, 
+        historical_oracle_data, 
+        last_acceptable_price
+    )?;
+
+    let is_oracle_valid = is_oracle_valid_for_action(
+        Some(Actions::FillOrder),
+        oracle_validity
+    )?;
+
+    let fills_paused = market.status == MarketStatus::FillsPaused;
+
+    let block = !is_oracle_valid || is_mark_price_too_divergent || fills_paused;
+
+    Ok(block)
+
+    // Ok(true)
+}
+
+/// Struct monitoring the current oracle status.
+/// Fields are the oracle price data, mark price divergence and oracle validity.
+/// Acts as a wrapper for the oracle price and oracle validity data structures
+#[derive(Default, Clone, Copy, Debug)]
+pub struct OracleStatus {
+    pub price_data: OraclePriceData,
+    pub oracle_mark_spread_pct: i64,
+    pub is_mark_price_too_divergent: bool,
+    pub oracle_validity: OracleValidity
+}
+
+/// Function to fetch Oracle status with an explicit lifetime 'a
+pub fn get_oracle_status<'a>(
+    oracle_price_data: &'a OraclePriceData,
+    guard_rails: &OracleGuardRails,
+    historical_oracle_data: &HistoricalPriceData,
+    last_acceptable_price: Option<u64>
+) -> SpedXSpotResult<OracleStatus> {
+    let oracle_validity = oracle_validity(
+        historical_oracle_data.last_oracle_twap,
+        oracle_price_data,
+        &guard_rails.validity
+    )?;
+
+    let oracle_mark_spread_pct = calculate_oracle_twap_5min_mark_spread_pct(
+        last_acceptable_price,
+        historical_oracle_data
+    )?;
+
+    let is_oracle_mark_too_divergent = is_oracle_mark_too_divergent(
+        oracle_mark_spread_pct, &guard_rails.price_divergence_guard_rails
+    )?;
+
+    Ok(
+        OracleStatus {
+            price_data: *oracle_price_data,
+            oracle_mark_spread_pct: oracle_mark_spread_pct,
+            is_mark_price_too_divergent: is_oracle_mark_too_divergent,
+            oracle_validity
+        }
+    )
+}
+
+/// Boolean function returning whether the mark price is too divergent from the oracle price. 
+/// Maximum allowed divergence - 10% of the precision
+pub fn is_oracle_mark_too_divergent(
+    price_spread_pct: i64,
+    oracle_mark_guard_rails: &PriceDivergenceGuardRails,
+) -> SpedXSpotResult<bool> {
+    let max_divergence = oracle_mark_guard_rails
+        .oracle_mark_pct_divergence
+        .max(PERCENTAGE_PRECISION_U64 / 10);
+
+    Ok(price_spread_pct.unsigned_abs() > max_divergence)
+}
+
+/// Function to calculate the spread between the oracle 5min twap and the mark price in %.
+pub fn calculate_oracle_twap_5min_mark_spread_pct(
+    last_acceptable_price: Option<u64>,
+    historical_oracle_data: &HistoricalPriceData
+) -> SpedXSpotResult<i64> {
+    let price = match last_acceptable_price {
+        Some(price) => price,
+        None => 0
+    };
+
+    let price_spread = price
+        .cast::<i64>()?
+        .safe_sub(historical_oracle_data.last_oracle_twap_5min)?;
+
+    price_spread
+        .cast::<i128>()?
+        .safe_mul(BID_ASK_SPREAD_PRECISION_I128)?
+        .safe_div(price.cast::<i128>()?)?
+        .cast()
 }
