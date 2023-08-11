6ebc8df03ebe68fda60bf717c4beb2b497400cb6
Karthik Pariti <karthik@spedx.exchange>
2023-08-11 21:23:26 +0530

    remove unused import

--- programs/spot_margin/src/state/config.rs
+++ programs/spot_margin/src/state/config.rs
@@ -4,11 +4,11 @@ use enumflags2::BitFlags;
 use crate::{
     error::SpedXSpotResult,
     math::{
-        constants::{
-            FEE_DENOMINATOR,
-            FEE_PERCENTAGE_DENOMINATOR,
-            PERCENTAGE_PRECISION_U64
-        },
+        // constants::{
+        //     FEE_DENOMINATOR,
+        //     FEE_PERCENTAGE_DENOMINATOR,
+        //     PERCENTAGE_PRECISION_U64
+        // },
         safe_unwrap::SafeUnwrap
     },
     state::{
