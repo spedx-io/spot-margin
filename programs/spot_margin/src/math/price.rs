20a21c8515ea3d41a04d4e20749f3dae2ad34b42
Karthik Pariti <karthik@spedx.exchange>
2023-08-19 22:13:34 +0530

    add fns to standardize base asset amt

--- programs/spot_margin/src/math/price.rs
+++ programs/spot_margin/src/math/price.rs
@@ -1,9 +1,6 @@
 //! Math utilities for price-related functions
 use crate::{
-    state::{
-        enums::PositionDirection,
-        // order::Order,
-    }, 
+    state::enums::PositionDirection, 
     error::{SpedXSpotResult, ErrorCode},
 };
 
@@ -52,3 +49,41 @@ pub fn standardize_price_i64(
         PositionDirection::TwoWay => price.safe_add(tick_size)
     }
 }
+
+pub fn standardize_base_asset_amt(
+    base_asset_amt: u64,
+    order_step_size: u64
+) -> SpedXSpotResult<u64> {
+    let remainder = base_asset_amt
+        .checked_rem_euclid(order_step_size)
+        .ok_or_else(|| ErrorCode::MathError)?;
+
+    base_asset_amt.safe_sub(remainder)
+}
+
+pub fn standardize_base_asset_amt_ceil(
+    base_asset_amt: u64,
+    order_step_size: u64
+) -> SpedXSpotResult<u64> {
+    let remainder = base_asset_amt
+        .checked_rem_euclid(order_step_size)
+        .ok_or_else(|| ErrorCode::MathError)?;
+
+    if remainder == 0 {
+        Ok(base_asset_amt)
+    } else {
+        base_asset_amt.safe_add(order_step_size)?.safe_sub(remainder)
+    }
+}
+
+pub fn is_base_asset_amt_multiple_of_order_step_size(
+    base_asset_amt: u64,
+    order_step_size: u64
+) -> SpedXSpotResult<bool> {
+    let remainder = base_asset_amt
+        .checked_rem_euclid(order_step_size)
+        .ok_or_else(|| ErrorCode::MathError)?;
+
+    Ok(remainder == 0)
+}
+
